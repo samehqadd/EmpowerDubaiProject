a3c05efe91d4957bd067eb040bfd16274e6f800e
Sameh Qaddoura <sam.qadd@gmail.com>
2023-10-25 14:16:01 +0300

    Add project files.

---
+++ EmpowerDubaiProject_1/EmpowerDubaiProject_1.cs
@@ -0,0 +1,25 @@
+namespace EmpowerDubaiProject_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Hello World!");
+			var element = engine.FindElement("Localhost");
+			engine.GenerateInformation("IP:" + element.PollingIP);
+		}
+	}
+}
